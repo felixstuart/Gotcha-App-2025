1c60067549344281634cbe041fa1df2e00ca4e1e
Yaman-Habip <yamanhabip@icloud.com>
2022-05-02 11:53:17 -0400

    Added Swift Files

---
+++ Gotcha App/Shared/Gotcha_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gotcha_AppApp.swift
+//  Shared
+//
+//  Created by Yaman Habip on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Gotcha_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
