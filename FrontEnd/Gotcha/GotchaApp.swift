13eed8c57aa788dfa6a543e34adde06969472a42
blankner <blake_ankner23@milton.edu>
2022-05-12 18:38:24 -0400

    Start on MainView

--- FrontEnd/Gotcha/GotchaApp.swift
+++ FrontEnd/Gotcha/GotchaApp.swift
@@ -6,9 +6,35 @@
 //
 
 import SwiftUI
+//import FirebaseCore
+//import Firebase
+
 
 @main
 struct GotchaApp: App {
+    
+//    init() {
+//        FirebaseApp.configure()
+//
+//        let db = Firestore.firestore()
+//
+//                db.collection("people").getDocuments() { (querySnapshot, err) in
+//                    if let err = err {
+//                        print("error with data: \(err)")
+//                    }
+//
+//                    else {
+//                        for document in querySnapshot!.documents{
+//                            print(document.data())
+//                        }
+//                    }
+//
+//                }
+//    }
+    
+    
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
--- FrontEnd/Gotcha/MainView.swift
+++ FrontEnd/Gotcha/MainView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        List {
-            .toolbar {
-                Button(action: <#T##() -> Void#>, label: <#T##() -> _#>)
+        VStack(alignment: .leading){
+            HStack{
+                Text("Name")
+                    .font(.headline)
+                Spacer()
+                Label("Leaderboard", systemImage: "crown.fill")
             }
+            Spacer()
+            Spacer()
+            Text("Your Target: ______")
+            Text("Number of Tages: ______")
+            Circle()
+            Text("Tag out")
+            
         }
+        .navigationTitle("Main Page")
+        .padding(4)
     }
 }
 
