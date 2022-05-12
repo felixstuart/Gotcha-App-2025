b6081d5b5bdbb1a5d9869c36b43ba12033079ceb
blankner <blake_ankner23@milton.edu>
2022-05-12 12:15:03 -0400

    Setup

---
+++ FrontEnd/Gotcha/LeaderBoardView.swift
@@ -0,0 +1,20 @@
+//
+//  LeaderBoardView.swift
+//  Gotcha
+//
+//  Created by Blake Ankner on 5/12/22.
+//
+
+import SwiftUI
+
+struct LeaderBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LeaderBoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        LeaderBoardView()
+    }
+}
---
+++ FrontEnd/Gotcha/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Gotcha
+//
+//  Created by Blake Ankner on 5/12/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
