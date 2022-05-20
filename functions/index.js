32298894899cedac9516c943fc6272e74f1575a4
JRyanShue <ryan_shue23@milton.edu>
2022-05-20 14:46:35 -0400

    Recommit...

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from the Gotcha 2022 Team!");
+});
+
+exports.tagOut = functions.https.onRequest((request, response) => {
+    functions.logger.info("Tag out logs!", {structuredData: true});
+    response.send("Tagging out...");
+});
