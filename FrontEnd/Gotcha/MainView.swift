b8efa00858316d350c139a4a3e7141c9cc8b1fde
blankner <blake_ankner23@milton.edu>
2022-05-19 21:17:46 -0400

    Fixed?

--- FrontEnd/Gotcha/MainView.swift
+++ FrontEnd/Gotcha/MainView.swift
@@ -14,7 +14,6 @@ struct MainView: View {
 //    @State private var engine: CHHapticEngine?
 //    @State private var leaderBoard: List = ["HEYYEH", "LWHEFL", "jf"]
     var body: some View {
-<<<<<<< HEAD
         TabView {
             ProfileView()
                 .tabItem {
@@ -25,105 +24,16 @@ struct MainView: View {
             TagOutView()
                 .tabItem {
                     Label("Tag Out", systemImage: "xmark.seal.fill")}
-=======
-        VStack{
-            HStack{
-                Text("Name")
-                    .font(.title)
-                Spacer()
-                Button(action: {}){
-                    Image(systemName: "crown.fill")
-                }
-//                Label("Leaderboard", systemImage: "crown.fill")
-//                    .font(.title2)
-            }
-            Spacer()
-         
-            Text("Your Target: ______")
-                .font(.title3)
-            Text("Number of Tages: ______")
-                .font(.title3)
-            Spacer()
-            Button(action: {
-                prepareHaptics()
-                youSureView = true
-                complexSuccess()
-                
-            }) {
-                Text("Tag Out")
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(Color.black)
-                    .background(Color("background"))
-                    .clipShape(Circle())
-                    .shadow(color: Color("lightShadow"), radius: 8, x: -8, y: -8)
-                    .shadow(color: Color("darkShadow"), radius: 8, x: 8, y: 8)
-            }
-        }
-        .sheet(isPresented: $youSureView) {
-            NavigationView{
-                TagOutView()
-            }
-        }
-        .navigationTitle("Main Page")
-        .padding(4)
-    }
-    
-//    hpatic functions
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
-        }
-    }
-
-    func complexSuccess() {
-        // make sure that the device supports haptics
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-
-        // create one intense, sharp tap
-        for i in stride(from: 0, to: 3, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-
-        for i in stride(from: 0, to: 3, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-
-        // convert those events into a pattern and play it immediately
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
->>>>>>> 31da5236263fc577feac83ca19762f75311941f7
         }
     }
 }
+
+
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
             .preferredColorScheme(.dark)
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-            .previewInterfaceOrientation(.portrait)
-            
->>>>>>> 31da5236263fc577feac83ca19762f75311941f7
     }
 }
 
