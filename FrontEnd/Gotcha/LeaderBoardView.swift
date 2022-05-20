b30f59e054524e5c9083c8a2b5382db7aa2359f6
Yaman-Habip <yamanhabip@icloud.com>
2022-05-20 16:13:36 -0400

    worked on leaderboard view

--- FrontEnd/Gotcha/LeaderBoardView.swift
+++ FrontEnd/Gotcha/LeaderBoardView.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 
-
 struct LeaderBoardView: View {
+    
+    struct Leader: Identifiable{
+        
+        let name: String
+        let tags: String
+        let id = UUID()
+
+    }
+    
+    //populate this list with Leaders in the given format
+    
+    var leaders: [Leader] = [
+        
+        Leader(name: "Player1", tags: "1"),
+        Leader(name: "Player2", tags: "2"),
+        Leader(name: "Player3", tags: "3"),
+        Leader(name: "Player4", tags: "4")
+        
+    ]
+    
+    @State private var ids = Set<UUID>()
+
     var body: some View {
-        Text("Hello, World!")
+        
+        List(leaders, selection: $ids) {
+                
+                Text($0.name + $0.tags)
+
+        }
     }
 }
+        
+
 
 struct LeaderBoardView_Previews: PreviewProvider {
     static var previews: some View {
