a42941aaa12fc07e10c79b0fc3b8be852b692907
blankner <blake_ankner23@milton.edu>
2022-05-20 22:09:09 -0400

    Profile UI

--- FrontEnd/Gotcha/ProfileView.swift
+++ FrontEnd/Gotcha/ProfileView.swift
@@ -15,9 +15,10 @@ struct ProfileView: View {
     
     
     var body: some View {
-        VStack (alignment: .center, spacing: 20){
+        VStack {
             HStack{
-                Text("Name")
+                Spacer()
+                Text("Blake")
                     .padding()
                     .font(.largeTitle)
                     .foregroundColor(Color("white"))
@@ -25,23 +26,30 @@ struct ProfileView: View {
                     .cornerRadius(20)
                 Spacer()
             }
+            Image("blakeProfile")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .frame(width: 150, height: 150)
+            
             Spacer()
-            Label("Your Target: _______", systemImage: "exclamationmark.shield.fill")
+            Label("Your Target: YAMAN HABIP", systemImage: "exclamationmark.shield.fill")
+                .multilineTextAlignment(.center)
                 .padding()
                 .font(.title2)
                 .foregroundColor(Color("white"))
-                .background(Color("firstBlue"))
+                .background(Color("mediumRed"))
                 .cornerRadius(20)
 //                .overlay(
 //                            RoundedRectangle(cornerRadius: 20)
 //                                .stroke(Color("acid"), lineWidth: 5)
 //                        )
                 
-            Label("Number of Tags: _______", systemImage: "number")
+            Label("Number of Tags: 5", systemImage: "number")
                 .font(.title2)
                 .padding()
                 .foregroundColor(Color("white"))
-                .background(Color("firstBlue"))
+                .background(Color("darkGreen"))
                 .cornerRadius(20)
             Spacer()
 //            ScrollView{
