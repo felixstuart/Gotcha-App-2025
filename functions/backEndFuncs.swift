f3ff7a7e35c8dc5bb85ca4aa4d443a4a58d90bc2
Andrew Rodriguez <andrewbrodriguez@gmail.com>
2022-05-26 11:29:28 -0400

    function file

---
+++ functions/backEndFuncs.swift
@@ -0,0 +1,98 @@
+//
+//  function.swift
+//  Gotcha
+//
+//  Created by Andrew Rodriguez on 5/26/22.
+//
+
+import Foundation
+import FirebaseFirestore
+
+func initFirebase(){
+    
+    let db = Firestore.firestore()
+    
+    let docRef = db.document("data/Andrew_Rodriguez23@milton.edu")
+
+
+    docRef.getDocument {snapshot, error in
+
+        guard let data = snapshot?.data(), error == nil else{
+            return
+        }
+        
+        print(data)
+        
+    
+    
+    }
+}
+
+func updateData(uid: String, field: String, data: Any) {
+    
+    let db = Firestore.firestore()
+    
+    let user = uid
+    
+    let docRef = db.document("data/" + user)
+    
+    docRef.updateData([field: data
+                   ])
+    
+}
+
+
+func tagOut(uid: String, lW: String){
+    //Reference to a specific firebase document within the collection users
+    let db = Firestore.firestore()
+    
+    let docRef = db.document("data/" + uid)
+    
+    //read the document and get a 'snapshot' of its current data , if it fails return
+    docRef.getDocument {snapshot, error in
+
+        guard let data = snapshot?.data(), error == nil else{
+            return
+        }
+        
+        print(data)
+        
+        guard let targ = data["target"] as? String else{
+            return
+        }
+        guard let chase = data["chaser"] as? String else{
+            return
+        }
+        guard let tags = data["tags"] as? Int else{
+            return
+        }
+        guard let alive = data["alive"] as? Bool else{
+            return
+        }
+        guard let lastWords = data["lastWords"] as? String else{
+            return
+        }
+
+        let personTaggingOut = uid
+        
+        updateData(uid: chase, field: "target", data: targ)
+        updateData(uid: chase, field: "tags", data: tags+1)
+        
+        updateData(uid: targ, field: "chaser", data: chase)
+        
+        updateData(uid: personTaggingOut, field: "alive", data: false)
+        updateData(uid: personTaggingOut, field: "lastWords", data: lW )
+    
+        
+        
+       
+        
+        
+       
+    
+    }
+    
+    
+
+    
+}
