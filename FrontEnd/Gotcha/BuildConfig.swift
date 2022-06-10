5bfc5c79a93b33d6f4dab0fc82316dc5d38d73c5
blankner <blake_ankner23@milton.edu>
2022-06-10 08:45:29 -0400

    USE THIS

---
+++ FrontEnd/Gotcha/BuildConfig.swift
@@ -0,0 +1,14 @@
+//
+//  BuildCnfig.swift
+//  Gotcha
+//
+//  Created by Blake Ankner on 6/9/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct BuildConfig {
+    static let googleKey = "YOUR_GOOGLE_CLIENT_ID"
+    
+}
