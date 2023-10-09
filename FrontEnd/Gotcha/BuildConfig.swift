ff6b6bd8c996983d2a856b8e907437ee30ea8ca3
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2023-10-08 23:32:51 -0400

    replaced security issue with proper key request

--- FrontEnd/Gotcha/BuildConfig.swift
+++ FrontEnd/Gotcha/BuildConfig.swift
@@ -3,11 +3,20 @@
 //  Gotcha
 //
 //  Created by Blake Ankner on 6/9/22.
-//
+//  Modifed by Ben Rhodes-Kropf on 10/8/23
 
 import Foundation
 import SwiftUI
 
 struct BuildConfig {
-    static let googleKey = "PUT KEY HERE!"
+    static var googleKey: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"),
+                  let plist = NSDictionary(contentsOfFile: filePath),
+                  let clientId = plist["CLIENT_ID"] as? String else {
+                fatalError("Couldn't fetch Google key from GoogleService-Info.plist.")
+            }
+            return clientId
+        }
+    }
 }
